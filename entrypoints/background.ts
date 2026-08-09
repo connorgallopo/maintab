03c0169a0d74730a00c01047fac4714cd6daddad
Connor Gallopo <connor.gallopo@me.com>
2026-08-09 13:59:45 -0400

    background polling and refresh messages

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -1,3 +1,26 @@
+import { browser } from '#imports';
+import { runCycle } from '../lib/cycle';
+import { configItem } from '../lib/storage';
+
 export default defineBackground(() => {
-  console.log('maintab background up');
+  const schedule = async () => {
+    const { pollMinutes } = await configItem.getValue();
+    await browser.alarms.create('poll', { periodInMinutes: Math.max(1, pollMinutes) });
+  };
+
+  browser.runtime.onInstalled.addListener(() => {
+    void schedule();
+    void runCycle();
+  });
+  browser.runtime.onStartup.addListener(() => {
+    void schedule();
+    void runCycle();
+  });
+  browser.alarms.onAlarm.addListener((alarm) => {
+    if (alarm.name === 'poll') void runCycle();
+  });
+  browser.runtime.onMessage.addListener((msg: { type?: string }) => {
+    if (msg?.type === 'refresh') void runCycle();
+  });
+  configItem.watch(() => void schedule());
 });
